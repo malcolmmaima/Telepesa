6d3abab27f7e16e1909fca55585df52932a28e3b
malcolm maima <malcolmmaima@gmail.com>
2025-06-27 18:13:14 +0300

    Temporarily disable caching and Redis dependency for testing purposes in user service

--- Backend/user-service/src/main/java/com/maelcolium/telepesa/user/UserServiceApplication.java
+++ Backend/user-service/src/main/java/com/maelcolium/telepesa/user/UserServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication(scanBasePackages = "com.maelcolium.telepesa")
 @EnableJpaAuditing
-@EnableCaching
+// @EnableCaching
 @EnableFeignClients
 @EnableTransactionManagement
 public class UserServiceApplication {
